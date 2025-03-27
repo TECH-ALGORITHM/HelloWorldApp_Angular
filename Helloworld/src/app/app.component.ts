e30f91e192b913002985fabf601cfa96ff6fd36d
Ambuj Singh <joy212ambuj@gmail.com>
2025-03-28 00:56:24 +0530

    two way binding

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -7,31 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "Helloworld";
-  imgUrl = "./assets/bridgelabzLogo.webp";
+  imgUrl = "assets/bridgelabzLogo.webp";
   url = "https://www.bridgelabz.com/";
-  nameError: string = "";
-  
-  private _userName: string = "";
+  userName: string="";
 
-  get userName(): string {
-    return this._userName;
-  }
-
-  set userName(value: string) {
-    if (value.length >= 3 && /^[A-Z][a-zA-Z]*$/.test(value)) {
-      this._userName = value;
-      this.nameError = "";
-    } else {
-      this.nameError = "Incorrect Name";
-    }
-  }
-
-  ngOnInit(): void {
+  ngOnInit(): void{
     this.title = "Hello from BridgeLabz!!!";
   }
 
   onClick($event: Event) {
-    console.log("Logo Clicked!", $event);
+    console.log("Save button is Clicked!", $event);
     window.open(this.url, "_blank");
   }
 }
